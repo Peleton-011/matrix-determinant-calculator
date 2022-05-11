97049d9c11e69ac6c3fb23c738599768478c5410
Peleton-011 <103774269+Peleton-011@users.noreply.github.com>
2022-05-11 18:23:40 +0200

    Buggy random parameter generator
    Add random parameter generator, does not work yet.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 
+import random
+
 # Returns x position in the grid for a specific index 
 def getX(index, size):
     return index%size
@@ -7,22 +9,45 @@ def getX(index, size):
 def getY(index, size):
     return int(index/size) 
 
+#Makes a random parameter to place in a slot
+def randomNumber(characters):
+    character = ""
+    if random.randint(0, 1):
+        character += "-"
+    while True:
+        if random.randint(0, 1):
+            break
+        character += str(random.randint(0, 9)
+    maxlen = len(characters)
+    random.randint(0,maxlen-1)
+    
+    
 # Takes input matrix. Mode 0 is automatic
 def getInput (mode = 0):
     if mode:
         return
+    characters = []
     matrix = []
     dim = 4
     index = 0x61
     for i in range(dim):
-        matrix.append([])
+        #matrix.append([])
+        next
         
-    for j in range(dim*dim):
-        
-        matrix[getX(j, dim)].append(chr(index))
-        index += 1
+    j = 0
     
-    return matrix
+    while j < (0x7a - 0x60):
+        characters.append(chr(index))#[getX(j, dim)].append(chr(index))
+        index += 1
+        j += 1
+    for l in range(dim):
+        matrix.append([])
+    for x in range(dim*dim):
+        matrix[getX(x, dim)].append(randomNumber(characters))
+        
+        
+    
+    return 
 def getDeterminant():
     return
 print(getInput())
