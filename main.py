ed49ff64531bcb19e59dcdfb9b2720287429ea5c
Peleton011 <62434025+Peleton011@users.noreply.github.com>
2022-05-15 14:30:46 +0200

    Update input function
    Fixed the input function and added a specific function to generate a random matrix

--- main.py
+++ main.py
@@ -1,55 +1,73 @@
-
 import random
 
-# Returns x position in the grid for a specific index 
+
+# Returns x position in the grid for a specific index
 def getX(index, size):
     return index % size
 
+
 # Returns y position in the grid for a specific index
 def getY(index, size):
-    return int(index / size) 
+    return int(index / size)
+
 
 # Makes a random parameter to place in a slot
 def randomNumber(characters):
     character = ""
     maxlen = len(characters) - 1
-    
+
     # Randomly choose the sign of the number
     if random.randint(0, 1):
         character += "-"
-    
+
     # Randomly add a number to the parameter
     while random.randint(0, 1):
-        character += str(random.randint(0, 9)
+        character += str(random.randint(0, 9))
 
     # Add a random character to the parameter
     character += characters[random.randint(0, maxlen)]
-    
-    
-# Takes input matrix. Mode 0 is automatic
-def getInput (mode = 0):
-    if mode:
-        return
-    characters = []
+
+
+# Makes randomly generated matrix
+def makeInput(size=random.randint(2, 5)):
     matrix = []
-    dim = 4
+    characters = []
     index = 0x61
-    for i in range(dim):
-        #matrix.append([])
-        next
-        
     j = 0
-    
-    while j < (0x7a - 0x60):
-        characters.append(chr(index))#[getX(j, dim)].append(chr(index))
-        index += 1
-        j += 1
-    for l in range(dim):
+
+    # Make legal character list
+    for j in range(0x7a - 0x60):
+        characters.append(chr(index + j))  # [getX(j, dim)].append(chr(index))
+
+    # Make all columns of the matrix
+    for i in range(size):
         matrix.append([])
-    for x in range(dim*dim):
-        matrix[getX(x, dim)].append(randomNumber(characters))
-        
-    return characters 
+
+    # Make a random number for each cell
+    for x in range(size * size):
+        matrix[getX(x, size)].append(randomNumber(characters))
+
+    return matrix
+
+
+# Takes input matrix.
+def getInput():
+    matrix = []
+    size = int(input("What size is the matrix? "))
+
+    # Make all columns for the input matrix
+    for i in range(size):
+        matrix.append([])
+
+    # Ask for each cell's value individually
+    for i in range(size * size):
+        inputPrompt = "Insert cell " + str(getX(i, size)) + " , " + str(getY(i, size))
+        matrix[getX(i, size)].append(input(inputPrompt))
+
+    return [matrix, size]
+
+
 def getDeterminant():
     return
-print(getInput())
+
+print(makeInput())
